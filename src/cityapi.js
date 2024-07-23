c4eca74209d6fe20306c75428f22a8b230437fed
JessieZ <jessie.zhouu3146@gmail.com>
2024-07-22 22:57:51 -0400

    initial commit yipeeeee

---
+++ src/cityapi.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '45db8af524mshc3de725324cf320p180fcfjsn7246f7ae2004',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "285c371b481d3fe4a7418ff470a9ed08"
